d114e1f3b2dbda2172011ade627ebc8d4ba533bd
sasank-chindirala <sasankchindirala@gmail.com>
2025-05-06 15:43:50 -0400

    - Async hybrid approach with double buffered flushing (testing - updated benchmark_hybrid_pgm_lipp.cc to branching binary search and 128 as error bound value)

--- benchmarks/benchmark_hybrid_pgm_lipp.cc
+++ benchmarks/benchmark_hybrid_pgm_lipp.cc
@@ -6,16 +6,12 @@
 
 template <typename Searcher>
 void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark, bool pareto, const std::vector<int>& params) {
-  if (!pareto) {
-    util::fail("HybridPGMLIPP requires pareto mode with fixed hyperparameters");
-  } else {
-    benchmark.template Run<HybridPGMLIPP<uint64_t, Searcher, 8>>();
-  }
+    benchmark.template Run<HybridPGMLIPP<uint64_t, Searcher, 128>>(params);
 }
 
 template <int record>
 void benchmark_64_hybrid_pgm_lipp(tli::Benchmark<uint64_t>& benchmark, const std::string& filename) {
-  benchmark.template Run<HybridPGMLIPP<uint64_t, LinearSearch<record>, 8>>();
+    benchmark.template Run<HybridPGMLIPP<uint64_t, BranchingBinarySearch<record>, 128>>();
 }
 
 INSTANTIATE_TEMPLATES_MULTITHREAD(benchmark_64_hybrid_pgm_lipp, uint64_t);
